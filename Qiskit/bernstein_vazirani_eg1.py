81ddd9b3047deed6f3c886627091cd19dabe6702
Vijayasri Iyer <thisisvij98@gmail.com>
2020-04-20 22:13:30 +0530

    Added bernstein vazirani example

---
+++ Qiskit/bernstein_vazirani_eg1.py
@@ -0,0 +1,28 @@
+from qiskit import *
+from qiskit.tools.visualization import plot_histogram
+
+#secret number
+secretnumber = '101001'
+#6+1 qubits, 6 classical bits
+circuit = QuantumCircuit(6+1, 6)
+circuit.h([0,1,2,3,4,5])
+circuit.x(6)
+circuit.h(6)
+
+circuit.barrier()
+
+#depicting 1 and 0 bits
+circuit.cx(5, 6)
+circuit.cx(3, 6)
+circuit.cx(0, 6)
+
+circuit.barrier()
+circuit.measure([0,1,2,3,4,5], [0,1,2,3,4,5])
+
+circuit.draw(output='mp1')
+
+simulator = Aer.get_backend('qasm_simulator')
+result = execute(circuit, backend = simulator, shots=1).result()
+counts = result.get_counts()
+
+print(counts)
